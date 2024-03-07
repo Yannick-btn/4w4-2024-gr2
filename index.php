2546c2302a6632659e4304272ca3d497aec46eb8
Bastien <e2070264@cmaisonneuve.qc.ca>
2024-03-07 16:06:32 -0500

    s5c2 fin debut labo

--- index.php
+++ index.php
@@ -1,39 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+/**
+ * Modele de base pour index.php
+ */
+?>
+<?php get_header(); ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thème gr2</title>
-    <link rel="stylesheet" href=" <?php echo get_template_directory_uri() . '/normalize.css'; ?>">
-    <link rel="stylesheet" href=" <?php echo get_template_directory_uri() . '/style.css'; ?>">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-
-</head>
-
-<body>
-    <div id="menu" class="global">
-        <div class="entete_menu">
-        <?php wp_nav_menu();?>
-    </div>
-    </div>
     <div id="entete" class="global">
-        <header class="entete__header">
+        <div class="entete__header">
             <h1 class="bgc-text">Thème du groupe #2</h1>
             <h2 class="bgc-text">4W4 - conception d'interface <span>et développement Web</span></h2>
             <h3 class="bgc-text">TIM - Collège de Maisonneuve</h3>
             <button class="entete__button">Événements</button>
-        </header>
-        <div class="vague">
-
-            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill" style="fill:var(--couleur-arriere-accueil)"></path>
-            </svg>
-
-
-        </div>
+</div>
+<?php get_template_part("gabarit/vague"); ?>
     </div>
     <div id="accueil" class="global">
         <?php  ?>
@@ -47,22 +26,7 @@
                         $sigle = substr($titre, 0, 7);
                         $pos_parentese = strpos($titre, '(');
                         $duree = substr($titre, $pos_parentese + 1 , - 1);
-                        $titre2 = substr($titre, 7, $pos_parentese - 7);
-
-
-
-                        // //selectioner le nombre dheur a laide de strpos()
-                        // // Trouver les positions de la parenthèse ouvrante et fermante
-                        // $ouvert = strpos($titre, '(');
-                        // $fermant = strpos($titre, ')');
-                        // // Si les parenthèses ouvrante et fermante sont trouvées
-                        // if ($ouvert !== false && $fermant !== false) {
-                        //     // Calculer la longueur de la sous-chaîne entre les parenthèses
-                        //     $longueur_sous_chaine = $fermant - $ouvert - 1;
-
-                        //     // Extraire la sous-chaîne entre les parenthèses
-                        //     $contenu_parentheses = substr($titre, $ouvert + 1, $longueur_sous_chaine);
-                        // }
+                        $titre = substr($titre, 7, $pos_parentese - 7);
 
 
 
@@ -95,11 +59,7 @@
             </blockquote>
             <button class="entete__button">Appuyer pour des chocolats</button>
         </section>
-        <div class="vague">
-            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill" style="fill:var(--couleur-arriere-footer)"></path>
-            </svg>
-        </div>
+        <?php get_template_part("gabarit/vague"); ?>
     </div>
     <div id="footer" class="global">
         <footer class="footer__section">
